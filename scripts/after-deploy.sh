14c4ff90fe3dfa3ae8a29bf8253bfaacdf7f33bc
yunjaeeun44 <yunmegan44@sookmyung.ac.kr>
2024-09-06 22:41:37 +0900

    test4

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -3,5 +3,9 @@ REPOSITORY=/home/ubuntu/build
 
 cd $REPOSITORY
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm
+
 npm install
 npm start src
